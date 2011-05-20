733ee3e57d8564893dd63e1f719dc5f6fc5e2f58
Saleem Shafi <saleemshafi@gmail.com>
2011-05-19 22:49:17 -0500

    Figured out how to convert a StorageItem to a MavenProject so i can analyze dependencies.  Version 2.0.9 of Maven artifacts added as dependencies since those were also in the "test" scope.

--- src/main/java/com/paypal/nexus/ReverseDependencyEventInspector.java
+++ src/main/java/com/paypal/nexus/ReverseDependencyEventInspector.java
@@ -1,10 +1,20 @@
 package com.paypal.nexus;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.component.annotations.Component;
 import org.sonatype.nexus.proxy.events.AbstractEventInspector;
 import org.sonatype.nexus.proxy.events.EventInspector;
 import org.sonatype.nexus.proxy.events.RepositoryItemEventDelete;
 import org.sonatype.nexus.proxy.events.RepositoryItemEventStore;
+import org.sonatype.nexus.proxy.item.StorageFileItem;
+import org.sonatype.nexus.proxy.item.StorageItem;
 import org.sonatype.plexus.appevents.Event;
 
 @Component( role = EventInspector.class, hint = "ReverseDependencyEventInspector" )
@@ -15,25 +25,51 @@ public class ReverseDependencyEventInspector extends AbstractEventInspector {
 	}
 
 	public void inspect(Event<?> evt) {
+		try {
 		if (evt instanceof RepositoryItemEventStore) {
 			onItemStore((RepositoryItemEventStore)evt);
 		} else {
 			onItemDelete((RepositoryItemEventDelete)evt);
 		}
-	}
-
-	private void onItemDelete(RepositoryItemEventDelete evt) {
-		String artifactName = evt.getItemUid().getPath();
-		if (artifactName.endsWith(".pom")) {
-			System.out.println(evt.getItem().getAttributes().keySet());
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 
-	private void onItemStore(RepositoryItemEventStore evt) {
-		String artifactName = evt.getItemUid().getPath();
-		if (artifactName.endsWith(".pom")) {
-			System.out.println(evt.getItem().getAttributes().keySet());
+	private void onItemDelete(RepositoryItemEventDelete evt) throws IOException {
+		StorageItem item = evt.getItem();
+		if (item instanceof StorageFileItem && item.getPath().endsWith(".pom")) {
+			InputStream input = ((StorageFileItem)item).getContentLocator().getContent();
+			MavenProject project = getMavenProject(input);
+			if (project != null) {
+			System.out.println("!!!!!!!!!!!! Got "+project.getGroupId()+":"+project.getArtifactId());
+			}
 		}
 	}
 
+	private void onItemStore(RepositoryItemEventStore evt) throws IOException {
+		StorageItem item = evt.getItem();
+		if (item instanceof StorageFileItem && item.getPath().endsWith(".pom")) {
+			InputStream input = ((StorageFileItem)item).getContentLocator().getContent();
+			MavenProject project = getMavenProject(input);
+			if (project != null) {
+				System.out.println("!!!!!!!!!!!! Got "+project.getGroupId()+":"+project.getArtifactId());
+			}
+		}
+	}
+	
+	private MavenProject getMavenProject(InputStream pomFile) {
+		Model model = null;
+		InputStreamReader reader = null;
+		MavenProject project = null;
+		MavenXpp3Reader mavenreader = new MavenXpp3Reader();
+		try {
+		    reader = new InputStreamReader(pomFile);
+		    model = mavenreader.read(reader);
+		    project = new MavenProject(model);
+			
+		}catch(Exception ex){ex.printStackTrace();}
+		return project;
+	}
+	
 }
