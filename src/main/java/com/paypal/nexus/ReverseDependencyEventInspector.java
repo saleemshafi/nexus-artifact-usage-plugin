4c2505320fe11fd9c08572d354f5a599d481ec77
Saleem Shafi <saleemshafi@gmail.com>
2011-05-22 22:23:18 -0500

    Started trying to get a real list of dependencies from the POM file. No luck so far.

--- src/main/java/com/paypal/nexus/ReverseDependencyEventInspector.java
+++ src/main/java/com/paypal/nexus/ReverseDependencyEventInspector.java
@@ -1,75 +1,112 @@
 package com.paypal.nexus;
 
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.List;
+import java.util.Set;
 
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.model.Dependency;
 import org.apache.maven.model.Model;
 import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
 import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.PlexusContainerException;
 import org.codehaus.plexus.component.annotations.Component;
+import org.codehaus.plexus.component.repository.exception.ComponentLookupException;
 import org.sonatype.nexus.proxy.events.AbstractEventInspector;
 import org.sonatype.nexus.proxy.events.EventInspector;
 import org.sonatype.nexus.proxy.events.RepositoryItemEventDelete;
 import org.sonatype.nexus.proxy.events.RepositoryItemEventStore;
 import org.sonatype.nexus.proxy.item.StorageFileItem;
 import org.sonatype.nexus.proxy.item.StorageItem;
+import org.sonatype.nexus.proxy.repository.HostedRepository;
 import org.sonatype.plexus.appevents.Event;
 
-@Component( role = EventInspector.class, hint = "ReverseDependencyEventInspector" )
+@Component(role = EventInspector.class, hint = "ReverseDependencyEventInspector")
 public class ReverseDependencyEventInspector extends AbstractEventInspector {
 
 	public boolean accepts(Event<?> evt) {
-		return evt instanceof RepositoryItemEventStore || evt instanceof RepositoryItemEventDelete;
+		return evt instanceof RepositoryItemEventStore
+				|| evt instanceof RepositoryItemEventDelete;
 	}
 
 	public void inspect(Event<?> evt) {
 		try {
-		if (evt instanceof RepositoryItemEventStore) {
-			onItemStore((RepositoryItemEventStore)evt);
-		} else {
-			onItemDelete((RepositoryItemEventDelete)evt);
-		}
+			if (evt instanceof RepositoryItemEventStore) {
+				onItemStore((RepositoryItemEventStore) evt);
+			} else {
+				onItemDelete((RepositoryItemEventDelete) evt);
+			}
 		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ComponentLookupException e) {
+			e.printStackTrace();
+		} catch (PlexusContainerException e) {
 			e.printStackTrace();
 		}
 	}
 
 	private void onItemDelete(RepositoryItemEventDelete evt) throws IOException {
-		StorageItem item = evt.getItem();
-		if (item instanceof StorageFileItem && item.getPath().endsWith(".pom")) {
-			InputStream input = ((StorageFileItem)item).getContentLocator().getContent();
-			MavenProject project = getMavenProject(input);
-			if (project != null) {
-			System.out.println("!!!!!!!!!!!! Got "+project.getGroupId()+":"+project.getArtifactId());
+		// we only care about artifacts added to hosted repos
+		if (evt.getRepository().getRepositoryKind()
+				.isFacetAvailable(HostedRepository.class)) {
+			// we only care about POM files
+			StorageItem item = evt.getItem();
+			if (item instanceof StorageFileItem
+					&& item.getPath().endsWith(".pom")) {
+				// convert to a Maven project
+				InputStream input = ((StorageFileItem) item)
+						.getContentLocator().getContent();
+				MavenProject project = getMavenProject(input);
+				if (project != null) {
+					Set<Artifact> dependencies = project.getDependencyArtifacts();
+					for (Artifact dependency : dependencies) {
+						System.out.println(project.getArtifactId()+" depends on "+dependency.getArtifactId());
+					}
+				}
 			}
 		}
 	}
 
-	private void onItemStore(RepositoryItemEventStore evt) throws IOException {
-		StorageItem item = evt.getItem();
-		if (item instanceof StorageFileItem && item.getPath().endsWith(".pom")) {
-			InputStream input = ((StorageFileItem)item).getContentLocator().getContent();
-			MavenProject project = getMavenProject(input);
-			if (project != null) {
-				System.out.println("!!!!!!!!!!!! Got "+project.getGroupId()+":"+project.getArtifactId());
+	private void onItemStore(RepositoryItemEventStore evt) throws IOException, ComponentLookupException, PlexusContainerException {
+		// we only care about artifacts added to hosted repos
+		if (evt.getRepository().getRepositoryKind()
+				.isFacetAvailable(HostedRepository.class)) {
+			// we only care about POM files
+			StorageItem item = evt.getItem();
+			if (item instanceof StorageFileItem
+					&& item.getPath().endsWith(".pom")) {
+				// convert to a Maven project
+				InputStream input = ((StorageFileItem) item)
+						.getContentLocator().getContent();
+				MavenProject project = getMavenProject(input);
+				if (project != null) {
+					Set<Artifact> dependencies = project.getDependencyArtifacts();
+					for (Dependency dependency : (List<Dependency>)project.getDependencies()) {
+						System.out.println(project.getArtifactId()+" depends on "+dependency.getGroupId()+":"+dependency.getArtifactId()+":"+dependency.getVersion());
+					}
+				}
+				
+			
 			}
 		}
 	}
-	
-	private MavenProject getMavenProject(InputStream pomFile) {
+
+	static MavenProject getMavenProject(InputStream pomFile) {
 		Model model = null;
 		InputStreamReader reader = null;
 		MavenProject project = null;
 		MavenXpp3Reader mavenreader = new MavenXpp3Reader();
 		try {
-		    reader = new InputStreamReader(pomFile);
-		    model = mavenreader.read(reader);
-		    project = new MavenProject(model);
-			
-		}catch(Exception ex){ex.printStackTrace();}
+			reader = new InputStreamReader(pomFile);
+			model = mavenreader.read(reader);
+			project = new MavenProject(model);
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 		return project;
 	}
-	
+
 }
---
+++ src/test/java/com/paypal/nexus/TestMavenProjectParsing.java
@@ -0,0 +1,36 @@
+package com.paypal.nexus;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Set;
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.factory.ArtifactFactory;
+import org.apache.maven.artifact.factory.DefaultArtifactFactory;
+import org.apache.maven.artifact.handler.manager.ArtifactHandlerManager;
+import org.apache.maven.artifact.handler.manager.DefaultArtifactHandlerManager;
+import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.util.ReflectionUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestMavenProjectParsing {
+	@Test
+	public void testMavenProjectDependencies() throws Exception {
+		// convert to a Maven project
+		InputStream input = new FileInputStream("pom.xml");
+		MavenProject project = ReverseDependencyEventInspector.getMavenProject(input);
+		Assert.assertNotNull(project);
+		ArtifactFactory factory = new DefaultArtifactFactory();
+		ArtifactHandlerManager handlerMgr = new DefaultArtifactHandlerManager();
+		ReflectionUtils.setVariableValueInObject(factory, "artifactHandlerManager", handlerMgr);
+		ReflectionUtils.setVariableValueInObject(handlerMgr, "artifactHandlers", new HashMap());
+		project.setDependencyArtifacts(project.createArtifacts(factory, null, null));
+		Set<Artifact> dependencies = project.getDependencyArtifacts();
+		Assert.assertNotNull(dependencies);
+		for (Artifact dependency : dependencies) {
+			System.out.println(project.getArtifactId()+" depends on "+dependency.getId());
+		}
+	}
+}
