1fc518c72fce475f19f30103ca6de5a1227c6fe8
Saleem Shafi <saleemshafi@gmail.com>
2011-06-04 23:38:30 -0500

    Added logic to automatically start the calculation of reverse dependencies wheneever the server starts up.

--- src/main/java/org/ebayopensource/nexus/reversedep/task/CalculateReverseDependenciesTask.java
+++ src/main/java/org/ebayopensource/nexus/reversedep/task/CalculateReverseDependenciesTask.java
@@ -2,12 +2,12 @@ package org.ebayopensource.nexus.reversedep.task;
 
 import org.codehaus.plexus.component.annotations.Component;
 import org.codehaus.plexus.component.annotations.Requirement;
+import org.codehaus.plexus.personality.plexus.lifecycle.phase.Initializable;
+import org.codehaus.plexus.personality.plexus.lifecycle.phase.InitializationException;
 import org.ebayopensource.nexus.reversedep.task.descriptors.ReverseDependencyCalculationTaskDescriptor;
-import org.sonatype.nexus.proxy.repository.HostedRepository;
-import org.sonatype.nexus.proxy.repository.Repository;
 import org.sonatype.nexus.scheduling.AbstractNexusRepositoriesTask;
+import org.sonatype.scheduling.Scheduler;
 import org.sonatype.scheduling.SchedulerTask;
-
 
 /**
  * Scheduled task to build reverse dependency mapping for all artifacts in a
@@ -21,11 +21,16 @@ import org.sonatype.scheduling.SchedulerTask;
  */
 @Component(role = SchedulerTask.class, hint = ReverseDependencyCalculationTaskDescriptor.ID, instantiationStrategy = "per-lookup")
 public class CalculateReverseDependenciesTask extends
-		AbstractNexusRepositoriesTask<ReverseDependencyCalculationResult> {
+		AbstractNexusRepositoriesTask<ReverseDependencyCalculationResult>
+		implements Initializable {
+
 	public static final String CALCULATE_REVERSE_DEPENDENCIES_ACTION = "CALCREVERSEDEPENDENCIES";
 
 	@Requirement
 	private ReverseDependencyCalculator calculator;
+
+	@Requirement
+	private Scheduler scheduler;
 
 	@Override
 	protected ReverseDependencyCalculationResult doRun() throws Exception {
@@ -34,6 +39,16 @@ public class CalculateReverseDependenciesTask extends
 				getRepositoryId(), getRepositoryGroupId());
 		this.calculator.calculateReverseDependencies(request);
 		return result;
+	}
+
+	public void initialize() throws InitializationException {
+		try {
+			this.scheduler.submit(getAction(), this);
+		} catch (Exception e) {
+			throw new InitializationException(
+					"Couldn't calculate reverse dependencies while initializing server",
+					e);
+		}
 	}
 
 	@Override
